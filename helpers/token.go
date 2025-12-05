c1385c66098911fb6ca853c6d93fc6cd0a8556fb
Ajijagatsaputra <ajijagat90@gmail.com>
2025-12-05 13:21:26 +0700

    feat: add endpoint myprofile

--- helpers/token.go
+++ helpers/token.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"fmt"
 	"go-jwt/models"
 	"time"
 
@@ -30,4 +31,22 @@ func CreateToken(user *models.User) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	ss, err := token.SignedString(mySigningKey)
 	return ss, err
+}
+
+func ValidateToken(tokenString string) (any, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return mySigningKey, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("unauthorized")
+	}
+
+	claims, ok := token.Claims.(*MyCustomClaims)
+
+	if !ok || !token.Valid {
+		return nil, fmt.Errorf("unauthorized")
+	}
+
+	return claims, nil
 }
